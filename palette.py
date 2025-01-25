8db67b8a37b50aa05fc0876ed66f81104f5da0eb
SearParsley <searparsley@gmail.com>
2025-01-24 22:17:33 -0700

    Create palette.py

---
+++ palette.py
@@ -0,0 +1,13 @@
+import curses
+
+
+colors = [
+  180, # light brown
+]
+
+def init_colors():
+  curses.start_color()
+
+  for i in range(len(colors)):
+    print(colors[i])
+    curses.init_pair(i+16, colors[i], curses.COLOR_BLACK)
