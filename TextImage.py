685dee235a646ebe48028fcb7db443ea3ec133a7
SearParsley <searparsley@gmail.com>
2025-01-25 03:15:02 -0700

    methods to get TextImage size

--- TextImage.py
+++ TextImage.py
@@ -5,13 +5,25 @@ class Text_Image:
     self.path = path
     self.window = window
     self.color = color
+    self.width = 0
+    self.height = 0
 
     with open(self.path, 'r') as file:
       self.text = file.read()
     
   def draw(self, start_x, start_y):
     for i, line in enumerate(self.text.splitlines()):
+      self.height += 1
+      if len(line) > self.width: self.width = len(line)
       self.window.addstr(i + start_y, start_x, line, self.color)
 
   def show(self):
     self.window.refresh()
+
+  def get_width(self):
+    return self.width
+
+  def get_height(self):
+    return self.height
+
+  
